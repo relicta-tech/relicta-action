66a347cff0074b8b1fab5c5d8a307cb3f547ac66
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-22 23:30:53 +0100

    style: format generated declaration files for prettier

--- dist/installer.d.ts
+++ dist/installer.d.ts
@@ -1,6 +1,10 @@
-export declare function installRelicta(version: string): Promise<string>;
+export declare function installRelicta(version: string): Promise<string>
 /**
  * Install specified plugins for the current platform.
  * Plugins are downloaded from the same release as the main binary.
  */
-export declare function installPlugins(version: string, plugins: string[], relictaBinaryPath: string): Promise<void>;
+export declare function installPlugins(
+  version: string,
+  plugins: string[],
+  relictaBinaryPath: string
+): Promise<void>
