4a1b934ede3b38bca135c7b157277e171345f7c9
Felix Geelhaar <felix@felixgeelhaar.de>
2025-12-23 01:59:12 +0100

    style: format generated declaration files

--- dist/installer.d.ts
+++ dist/installer.d.ts
@@ -1,6 +1,10 @@
-export declare function installRelicta(version: string): Promise<string>;
+export declare function installRelicta(version: string): Promise<string>
 /**
  * Install specified plugins for the current platform.
  * Each plugin is downloaded from its own repo: relicta-tech/plugin-{name}
  */
-export declare function installPlugins(version: string, plugins: string[], relictaBinaryPath: string): Promise<void>;
+export declare function installPlugins(
+  version: string,
+  plugins: string[],
+  relictaBinaryPath: string
+): Promise<void>
